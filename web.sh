d8e71d7a7ae553b9debac62ad856493fb3ade87e
msgithub-111 <75408443+msgithub-111@users.noreply.github.com>
2021-03-03 17:11:17 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -9,6 +9,9 @@ sudo apt-get install -y apache2
 #yum update –y
 #yum install –y apache2
 
+sudo chmod 777 /var/www/html
+sudo chmod 777 /var/www/html/index.html
+
 echo "<html>" > /var/www/html/index.html   ## 리다이렉션 반드시 한개로
 echo "Hello World from $(hostname -f)" >> /var/www/html/index.html
 echo "</html>" >> /var/www/html/index.html
