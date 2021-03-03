1335b916298c4659af81153e34fec49bedfc6460
msgithub-111 <75408443+msgithub-111@users.noreply.github.com>
2021-03-03 12:59:31 +0900

    Add files via upload

--- vm2.tf
+++ vm2.tf
@@ -4,13 +4,13 @@ resource "azurerm_virtual_machine" "web2" {
     resource_group_name   = azurerm_resource_group.user23-rg.name
     availability_set_id   = azurerm_availability_set.avset.id
     network_interface_ids = [azurerm_network_interface.nic2.id]
-    vm_size               = "Standard_DS1_v2"
+    vm_size               = "Standard_B1ms"
 
     storage_os_disk {
         name              = "myOsDisk2"
         caching           = "ReadWrite"
         create_option     = "FromImage"
-        managed_disk_type = "Premium_LRS"
+        managed_disk_type = "Standard_LRS"
     }
     storage_image_reference {
         publisher = "Canonical"
